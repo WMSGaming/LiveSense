2d55de9281eb584e720cb4c5e08ffcef5ceb2843
WMSGaming <lmdshad70@gmail.com>
2022-11-04 23:46:33 +0000

    Added HasteSpoof and fixed nofall

--- src/main/java/livesense/livesense/mixin/KeyboardMixin.java
+++ src/main/java/livesense/livesense/mixin/KeyboardMixin.java
@@ -3,6 +3,8 @@ package livesense.livesense.mixin;
 import livesense.livesense.LiveSense;
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.gui.screen.Screen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +15,9 @@ public class KeyboardMixin {
     @Inject(method = "onKey", at = @At("HEAD"), cancellable = true)
     public void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
 
-        LiveSense.INSTANCE.onKeyPress(key, action);
+        if(!(MinecraftClient.getInstance().currentScreen instanceof ChatScreen)) {
+            LiveSense.INSTANCE.onKeyPress(key, action);
+        }
 
     }
 }
